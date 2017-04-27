eea000c4da9548dabc0b3dc502de8d0cbd60e073
Alyssa Moody and kimlan nguyen <alyssa@>
2017-04-27 10:33:52 -0700

    Create style2 branch with alert for height restrictions

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 
   if (height >= 5) {
     $(".park-policies").show();
-    $("#park-policies, #loop-di-loop, #drop-of-death, #fun-wheel, #teacup-twirl").show();
 
     $("#park-policies").click(function() {
       $(".park-policies").fadeIn();
@@ -30,20 +29,41 @@ $(document).ready(function(){
       $(".family-ride-2").fadeIn();
     });
 
+    $("#crazy-bus").click(function() {
+      alert("Sorry, you are not short enough for this ride!");
+    });
+
+
   }
   else {
     $(".park-policies"). show();
 
+    $("#loop-di-loop").click(function() {
+      alert("Sorry, you are not tall enough!");
+    });
+
+    $("#drop-of-death").click(function() {
+      alert("Sorry, you are not tall enough!");
+    });
+
+
     $("#park-policies").click(function() {
+      $(".park-policies").siblings().hide();
       $(".park-policies").fadeIn();
     });
+
     $("#crazy-bus").click(function() {
+      $(".kid-ride-1").siblings().hide();
       $(".kid-ride-1").fadeIn();
     });
+
     $("#fun-wheel").click(function() {
+      $(".family-ride-1").siblings().hide();
       $(".family-ride-1").fadeIn();
     });
+
     $("#teacup-twirl").click(function() {
+      $(".family-ride-2").siblings().hide();
       $(".family-ride-2").fadeIn();
     });
   }
